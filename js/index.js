602414405058024c4a569bb18cfbd4727b661145
jwong <jwong@hotpads.com>
2014-06-09 18:31:11 -0700

    adding quiz logic via javascript

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// define results object to display user result
+
+var results = {
+	"SV" : 0,
+	"Mission": 0,
+	"Sunset": 0,
+	"GGPark": 0,
+	"Twin": 0,
+	"SOMA": 0,
+	"FiDi": 0,
+	"Tenderloin": 0,
+	"Presidio": 0,
+	"Castro": 0,
+	"Dogpatch": 0,
+	"Oakland": 0
+};
+
+// define function to scroll to next question
+
+var scrollNext = function () {
+	var currentid = this.id;
+}
+
+// click handlers to add points to results object
+
+$("#q01_wall").click( function() {
+	results.SV += 1;
+	scrollNext();
+	getMax();
+});
+
+// find max value from results object
+
+var getMax = function () {
+	var keyArray = Object.keys(results);
+	var valueArray = keyArray.map( function(key) {
+		return results[key];
+	});
+	var maxValue = Math.max.apply(null, valueArray);
+	console.log(valueArray);
+}
